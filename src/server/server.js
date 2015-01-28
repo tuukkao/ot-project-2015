adf78b453eb94533e6f24301df666ee12e159abd
Tuukka Ojala <tuukka.ojala@cs.tamk.fi>
2015-01-28 02:18:23 +0200

    Use BodyParser
    Add Body-parser to the app's list of middlewares. All request bodies coming in will now be parsed as json.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var expressApp = express();
+var bodyParser = require('body-parser');
 
 require('./routes')(expressApp);
+expressApp.use(bodyParser.json);
 
 expressApp.listen(8080);
