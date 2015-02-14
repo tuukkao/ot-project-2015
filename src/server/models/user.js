78bb65a4664977f3d9139b933045b47e27728f37
Pyry Nikunen <pyry.nikunen@cs.tamk.fi>
2015-02-14 16:15:52 +0200

    Modify user model for token based authentication.

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,10 +1,14 @@
 var mongoose = require("mongoose");
 var userSchema = mongoose.Schema({
     username: { type: String, unique: true, required: true, index: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    token: { type: String },
     display_name: { type: String, required: true },
     description: String,
     created_at: { type: Date, default: Date.now },
-    blogs: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Blog' } ]
+    blogs: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Blog' } ],
+    blogs_followed: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Blog' } ]
 });
 
 module.exports = mongoose.model('User', userSchema);
