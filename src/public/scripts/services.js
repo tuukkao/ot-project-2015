0d3faa5de42328aa729571ce4685e4d4aa03f32f
Pyry Nikunen <pyry.nikunen@cs.tamk.fi>
2015-02-15 15:44:38 +0200

    Add feedController function for fetching posts.
    FeedController will now fetch all posts for the blogs also.

--- src/public/scripts/services.js
+++ src/public/scripts/services.js
@@ -3,3 +3,19 @@ var app = angular.module('app');
     app.factory('Blogs', ['$http', 'ENV', function($http, ENV) {
         return $http.get(ENV.apiEndpoint + '/blogs');
     }])
+
+    app.factory('Posts', ['$http', 'ENV', function($http, ENV) {
+        return {
+            fetchPosts: function(blogId) {
+                return $http.get(ENV.apiEndpoint + '/blog/'+blogId+'/posts', { blogid: blogId});
+            }
+        }
+    }])
+
+    app.factory('Authorization', ['$http', 'ENV', function($http, ENV) {
+        return {
+            authenticate: function(credentials) {
+                return $http.post(ENV.apiEndpoint + '/login', credentials);
+            }
+        }
+    }])
